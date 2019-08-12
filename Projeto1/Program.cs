fdb9b0b4b86b61951127c24d6a9027b7d5568692
Natan Fraga <natanfraga@live.com>
2019-08-12 17:44:53 -0300

    Acrescentada a pergunta sobre como vc está

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -5,6 +5,7 @@ namespace Projeto1 {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como vc está hoje? ");
         }
     }
 }
