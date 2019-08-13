439a9d46d54a2996c76d2b6f5daef0ec1b95215a
Natan Fraga <natanfraga@live.com>
2019-08-13 14:40:13 -0300

    Acrescentada mensagem desejando um bom dia

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -4,8 +4,8 @@ namespace Projeto1 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
             Console.WriteLine("Como vc está hoje? ");
+            Console.WriteLine("Tenha um bom dia!");
         }
     }
 }
